ca4c916f51b7d6358f6340b54307295fc75c030b
Vladimir <57016594+VlTok@users.noreply.github.com>
2020-04-23 20:39:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-
+    
     $messages = array();
-
+    
     if (!empty($_COOKIE['save'])) {
-        setcookie('save','',100000);
-
-        echo '<div class="jumbotron w-25 p-3 mx-auto bg-success my-2">Thank you, saved</div>';
+       
+        $secret = array();
+        if (isset($_COOKIE['login'])){
+            $secret['log']=$_COOKIE['login'];
+            $secret['pas']=$_COOKIE['pass'];
+        }
+        setcookie('save', '', 100000);
+        setcookie('login', '', 100000);
+        setcookie('pass', '', 100000);
+        
+        echo '<div class="jumbotron w-25 p-3 mx-auto my-2 bg-success">Saved</div>';
+        
+        if (!empty($_COOKIE['pass'])) {
+            echo '<div class="jumbotron w-25 p-3 mx-auto my-2 bg-success">Login: ',$secret['log'],'</div>';
+            echo '<div class="jumbotron w-25 p-3 mx-auto my-2 bg-success">Password: ',$secret['pas'],'</div>';
+        }
     }
     $errors = array();
     $errors['Name'] = !empty($_COOKIE['Name_error']);
@@ -18,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $errors['SP'] = !empty($_COOKIE['SP_error']);
     $errors['BG'] = !empty($_COOKIE['BG_error']);
     $errors['CH'] = !empty($_COOKIE['CH_error']);
-
+    
     // Выдаем сообщения об ошибках.
     if ($errors['Name']) {
         // Удаляем куку, указывая время устаревания в прошлом.
@@ -28,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
     $values = array();
     $values['Name'] = empty($_COOKIE['Name_value']) ? '' : $_COOKIE['Name_value'];
-
+    
     if ($errors['Email']) {
         // Удаляем куку, указывая время устаревания в прошлом.
         setcookie('Email_error', '', 100000);
@@ -36,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $messages[] = '<div class="error">Enter email.</div>';
     }
     $values['Email'] = empty($_COOKIE['Email_value']) ? '' : $_COOKIE['Email_value'];
-
+    
     if ($errors['BG']) {
         // Удаляем куку, указывая время устаревания в прошлом.
         setcookie('BG_error', '', 100000);
@@ -44,7 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $messages[] = '<div class="error">Enter day.</div>';
     }
     $values['BG'] = empty($_COOKIE['BG_value']) ? '' : $_COOKIE['BG_value'];
-
+    
     if ($errors['DD']) {
         // Удаляем куку, указывая время устаревания в прошлом.
         setcookie('DD_error', '', 100000);
@@ -52,7 +65,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $messages[] = '<div class="error">Enter day.</div>';
     }
     $values['DD'] = empty($_COOKIE['DD_value']) ? '' : $_COOKIE['DD_value'];
-
+    
     if ($errors['DM']) {
         // Удаляем куку, указывая время устаревания в прошлом.
         setcookie('DM_error', '', 100000);
@@ -60,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $messages[] = '<div class="error">Enter month.</div>';
     }
     $values['DM'] = empty($_COOKIE['DM_value']) ? '' : $_COOKIE['DM_value'];
-
+    
     if ($errors['DY']) {
         // Удаляем куку, указывая время устаревания в прошлом.
         setcookie('DY_error', '', 100000);
@@ -68,14 +81,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $messages[] = '<div class="error">Enter year.</div>';
     }
     $values['DY'] = empty($_COOKIE['DY_value']) ? '' : $_COOKIE['DY_value'];
-
+    
     if ($errors['SP']){
         setcookie('SP_error', '', 100000);
         // Выводим сообщение.
         $messages[] = '<div class="error">Enter superpower.</div>';
     }
-    $values['SP'] = array();
-
+    $values['SP']=array();
+    if (key_exists("SP_value",$_COOKIE)) $values['SP'] = unserialize($_COOKIE['SP_value'], ["allowed_classes" => false]);
+    
     if ($errors['BG']) {
         // Удаляем куку, указывая время устаревания в прошлом.
         setcookie('BG_error', '', 100000);
@@ -83,22 +97,57 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $messages[] = '<div class="error">Enter biography.</div>';
     }
     $values['BG'] = empty($_COOKIE['BG_value']) ? '' : $_COOKIE['BG_value'];
-
+    
     if ($errors['CH']) {
-        // Удаляем куку, указывая время устаревания в прошлом.
         setcookie('CH_error', '', 100000);
         // Выводим сообщение.
         $messages[] = '<div class="error">Enter contract.</div>';
     }
     $values['CH'] = empty($_COOKIE['CH_value']) ? '' : $_COOKIE['CH_value'];
-
+    
     if (key_exists("PO_value",$_COOKIE)) $values['PO'] = $_COOKIE['PO_value']; else $values['PO'] = "MALE";
     if (key_exists("LI_value",$_COOKIE)) $values['LI'] = $_COOKIE['LI_value']; else $values['LI'] = "0";
-    if (key_exists("SP_value",$_COOKIE)) $values['SP'] = unserialize($_COOKIE['SP_value'], ["allowed_classes" => false]);
-
+    
+    if (!empty($_COOKIE[session_name()]) && session_start() && !empty($_SESSION['login'])) {
+        $user = 'u16342';
+        $pass = '7387652';
+        $db = new PDO('mysql:host=localhost;dbname=u16342', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+        
+        try {
+            $stmt = $db->prepare("SELECT NAME FROM formOne WHERE NAME=:name AND PASS=:upass");   //добавление в базу данные
+            $stmt -> execute(array('name'=>$_SESSION['login'], 'upass'=>md5($_SESSION['pass'])));
+            $base=$stmt->fetch();
+            print_r($base);
+            $values['SP'] = array();
+            $values['Name'] = $base['NAME'];
+            $values['Email'] = $base['EMAIL'];
+            $values['DD'] = $base['DAY'];
+            $values['DM'] = $base['MONTH'];
+            $values['DY'] = $base['YEAR'];
+            $values['BG'] = $base['BIO'];
+            $values['PO'] = $base['SEX'];
+            $values['LI'] = $base['NoL'];
+        }
+        catch(PDOException $e){
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+    }
     include('form.php');
 }
 else {
+    if($_POST['requestMe']=='LO') {
+        session_start();
+        $_SESSION=array();
+        session_destroy();
+        header('Location: index.php');
+        exit();
+    }
+    if($_POST['requestMe']=='LI'){
+        header('Location: ./login.php');
+        exit();
+    }
+    
     $errors = FALSE;
     if (empty($_POST['Name'])) {
         setcookie('Name_error', '1', time() + 24 * 60 * 60);
@@ -167,11 +216,11 @@ else {
     } else {
         setcookie('SP_value', serialize($_POST['SP']), time() + 30 * 24 * 60 * 60);
     }
-
+    
     if($errors) {
-
+        
         header('Location: index.php');
-
+        
         exit();
     }
     else {
@@ -186,21 +235,37 @@ else {
     }
     $sp='';
     for($i=0;$i<count($_POST['SP']);$i++){
-    $sp .= $_POST['SP'][$i] . '  ';
+        $sp .= $_POST['SP'][$i] . '  ';
     }
     $user = 'u16342';
     $pass = '7387652';
     $db = new PDO('mysql:host=localhost;dbname=u16342', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-
-    try {
-        $stmt = $db->prepare("INSERT INTO formOne (NAME,EMAIL,YEAR,SEX,Nol,SUPERPOWERS,BIO,CHECKBOX) VALUES (:NAME,:EMAIL,:YEAR,:SEX,:NoL,:SUPERPOWERS,:BIO,:CHECKBOX)");   //добавление в базу данные
-        $stmt -> execute(array('NAME'=>$_POST['Name'], 'EMAIL'=>$_POST['Email'],'YEAR'=>$_POST['DY'],'SEX'=>$_POST['Rad'],'NoL'=>$_POST['Limbs'], 'SUPERPOWERS'=>$sp, 'BIO'=>$_POST['BG'], 'CHECKBOX'=>$ch));
+    
+    try{
+        if (!empty($_COOKIE[session_name()])&
+            session_start() && !empty($_SESSION['login'])) {
+                
+                $stmt = $db->prepare("UPDATE formOne SET NAME=:name,DAY=:day,MONTH=:month,YEAR=:year, SEX=:sex, NoL=:nol, SUPERPOWERS=:superpowers, BIO=:bio, CHECKBOX=:checkbox, COMMIT_TIME=FROM_UNIXTIME(:time) WHERE PASS=:upass");   //обновление в базу данные
+                $stmt -> execute(array('name'=>$_POST['Name'],'day'=>$_POST['DD'],'month'=>$_POST['DM'],'year'=>$_POST['DY'], 'sex'=>$_POST['Rad'],'superpowers'=>$sp,'nol'=>$_POST['Limbs'],'bio'=>$_POST['BG'],'checkbox'=>$ch, 'upass'=>md5($_SESSION['pass']),'time'=>time()));
+                
+            }
+            else {
+                $login = $_POST['Email'];
+                $upass = rand(0,10000);
+                // Сохраняем в Cookies.
+                setcookie('login', $login);
+                setcookie('pass', $upass);
+                
+                $stmt = $db->prepare("INSERT INTO formOne (NAME,EMAIL,PASS,DAY,MONTH,YEAR,SEX,Nol,SUPERPOWERS,BIO,CHECKBOX) VALUES (:NAME,:EMAIL,:PASS,:DAY,:MONTH,:YEAR,:SEX,:NoL,:SUPERPOWERS,:BIO,:CHECKBOX)");   //добавление в базу данные
+                $stmt -> execute(array('NAME'=>$_POST['Name'], 'EMAIL'=>$_POST['Email'],'PASS'=>md5($upass),'DAY'=>$_POST['DD'],'MONTH'=>$_POST['DM'],'YEAR'=>$_POST['DY'],'SEX'=>$_POST['Rad'],'NoL'=>$_POST['Limbs'], 'SUPERPOWERS'=>$sp, 'BIO'=>$_POST['BG'], 'CHECKBOX'=>$ch));
+                
+            }
     }
     catch(PDOException $e){
-    print('Error : ' . $e->getMessage());
-    exit();
+        print('Error : ' . $e->getMessage());
+        exit();
     }
     setcookie('save','1');
-    header('Location: index.php');
+    header('Location: ./');
 }
 ?>
